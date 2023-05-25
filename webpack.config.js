d40cd57f883305f67fd31a0fb5b5ff719a7cf104
joseph-neeraj <joseph.neeraj@gmail.com>
2023-05-25 11:15:07 +0100

    upgrade to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  target: 'node',
   entry: './index.js',
   output: {
     filename: 'client-encryption-nodejs.min.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'mcencrypt',
     libraryTarget: 'var'
-
   },
-  node: {
-    fs: "empty"
-  }
 };
