cc7ada5007bd26f8306bddb36a75b4d828fcc827
karen-avetisyan-mc <117922723+karen-avetisyan-mc@users.noreply.github.com>
2024-08-06 14:03:22 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,19 @@ module.exports = {
     __filename: false,
     __dirname: false,
   },
-  
+  resolve: {
+  modules: [...],
+  fallback: {
+    "fs": false,
+    "tls": false,
+    "net": false,
+    "path": false,
+    "zlib": false,
+    "http": false,
+    "https": false,
+    "stream": false,
+    "crypto": false,
+    "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+  } 
+},
 };
